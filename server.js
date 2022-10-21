6984dec2077a847a264cb2eed967402e1626f3ec
AdiGunturu <75342181+adigunturu@users.noreply.github.com>
2022-10-21 22:36:09 +0530

    temp commit 4

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 var path = require('path');
 app.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname,'/public/index.html'));
 });
-app.listen(4000, function () {
+app.listen(process.env.PORT || 4000, function () {
     console.log('Node app is working!');
 });
