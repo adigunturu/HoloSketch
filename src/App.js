fe8efa9adbff215a7e0c79c2acedd8c0db3214b6
AdiGunturu <75342181+adigunturu@users.noreply.github.com>
2022-10-31 21:12:57 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SketchSpace from './SketchSpace';
+
+function App() {
+  return (
+    <div style={{width:'100vw', height:'100vh'}}>
+      <SketchSpace />
+    </div>
+  );
+}
+
+export default App;
